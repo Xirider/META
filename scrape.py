fcc6fd0812c96ac97212086d61f037e8cee99151
Xirid <peter.k.albert@gmail.com>
2019-05-24 21:06:16 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -25,34 +25,38 @@ def searchandsplit(query, maxchars = 10000, minparalen = 40, maxparalen = 700):
 
     for url in urllist:
         #print(url)
-        article = Article(url)
+        article = Article(url, fetch_images = False, memoize_articles=False)
         articlelist.append(article)
 
     news_pool.set(articlelist, threads_per_source = 1)
     news_pool.join()
 
     downloadtime = time.time() - start_time
-
+    print(f"Downloading finished after {downloadtime} seconds")
     paralist = []
     for article in articlelist:
-        article.parse()
-        body = article.text
-        body = body[:maxchars]
+        try:
+            article.parse()
+            body = article.text
+            body = body[:maxchars]
 
-        splitbody = body.split("\n\n")
-        
-        for para in splitbody:
-            sents = sent_tokenize(para)
-            for i in range(len(sents)):
-                string = ""
-                for x in range(len(sents)-i):
+            splitbody = body.split("\n\n")
+            
+            for para in splitbody:
+                sents = sent_tokenize(para)
+                for i in range(len(sents)):
+                    string = ""
+                    for x in range(len(sents)-i):
 
-                    if len(string) + len(sents[i + x]) > maxparalen:
-                        break
-                    string = "".join([string, sents[i + x]])
-                if len(string) >= minparalen and string not in paralist:
-                    paralist.append(string)
-    
+                        if len(string) + len(sents[i + x]) > maxparalen:
+                            break
+                        string = "".join([string, sents[i + x]])
+                    if len(string) >= minparalen and string not in paralist:
+                        paralist.append(string)
+        except:
+            continue
+    processingtime = time.time() - start_time
+    print(f"Processing finished after {processingtime}")
     return paralist
 
                 
