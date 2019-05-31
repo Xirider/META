5b3e4a9aeea989fa832324be7de81c9f20354e4e
Xirid <peter.k.albert@gmail.com>
2019-05-31 11:51:38 +0200

    Update run_nq.py

--- run_nq.py
+++ run_nq.py
@@ -741,20 +741,20 @@ def convert_single_example(example, tokenizer, is_training):
 
     # Pytorch test
 
-    import pdb; pdb.set_trace()
-    import torch
-    import modelingpt
-    # bertconfigpt = modelingpt.BertConfig.from_json_file("bert-joint-baseline/bert_config.json")
-    # ptmodel = modelingpt.BertNQA(bertconfigpt)
-    ptmodel = modelingpt.BertNQA.from_pretrained("bert-joint-baseline")
+    # import pdb; pdb.set_trace()
+    # import torch
+    # import modelingpt
+    # # bertconfigpt = modelingpt.BertConfig.from_json_file("bert-joint-baseline/bert_config.json")
+    # # ptmodel = modelingpt.BertNQA(bertconfigpt)
+    # ptmodel = modelingpt.BertNQA.from_pretrained("bert-joint-baseline")
 
 
-    ptmodel = ptmodel.cuda()
-    input_ids = torch.tensor(input_ids, device="cuda").unsqueeze(0)
-    token_type_ids = torch.tensor(segment_ids, device="cuda").unsqueeze(0)
-    attention_mask = torch.tensor(input_mask, device="cuda").unsqueeze(0)
+    # ptmodel = ptmodel.cuda()
+    # input_ids = torch.tensor(input_ids, device="cuda").unsqueeze(0)
+    # token_type_ids = torch.tensor(segment_ids, device="cuda").unsqueeze(0)
+    # attention_mask = torch.tensor(input_mask, device="cuda").unsqueeze(0)
 
-    start_logits, end_logits, answer_type_logits = ptmodel(input_ids = input_ids,  token_type_ids=token_type_ids, attention_mask=attention_mask)
+    # start_logits, end_logits, answer_type_logits = ptmodel(input_ids = input_ids,  token_type_ids=token_type_ids, attention_mask=attention_mask)
 
 
 
