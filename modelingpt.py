872a41ef17ab85862b80b74f467ff539172df2a0
Xirid <peter.k.albert@gmail.com>
2019-05-31 02:18:15 +0200

    Update modelingpt.py

--- modelingpt.py
+++ modelingpt.py
@@ -1310,7 +1310,7 @@ class BertNQA(BertPreTrainedModel):
 
         self.apply(self.init_bert_weights)
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None, , answer_type=None):
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None, answer_type=None):
         sequence_output, pooler = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
         logits = self.qa_outputs(sequence_output)
         answer_type_logits = self.answer_type(pooler)
