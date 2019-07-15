3d1568c089388901576b817e48e2ebc27a617eda
Xirid <peter.k.albert@gmail.com>
2019-07-15 22:09:23 +0200

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -540,7 +540,8 @@ def main():
             tr_loss = 0
             number_of_epochs += 1
             nb_tr_examples, nb_tr_steps = 0, 0
-            evaluate(number_of_epochs=number_of_epochs)
+            if number_of_epochs % 10 == 0:
+                evaluate(number_of_epochs=number_of_epochs)
             model.train()
             for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])):
                 batch = tuple(t.to(device) for t in batch)
