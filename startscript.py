201d8aa7077a2a1afc413c7cca86e6048064c70e
Xirid <peter.k.albert@gmail.com>
2019-08-30 12:27:46 +0200

    Update startscript.py

--- startscript.py
+++ startscript.py
@@ -1,7 +1,7 @@
 import os
 import time
 import subprocess
-subprocess.run(r'activate qa && cd C:/Users/sophi/Desktop/peter/qa/inference && python app.py && start "" "http://127.0.0.1:5000/"', shell=True)
+subprocess.run(r"activate qa && cd C:/Users/sophi/Desktop/peter/qa/inference && python app.py", shell=True)
 # os.system("activate qa")
 # time.sleep(5)
 # exe_str = r"cd C:/Users/sophi/Desktop/peter/qa/inference"
@@ -10,6 +10,6 @@ subprocess.run(r'activate qa && cd C:/Users/sophi/Desktop/peter/qa/inference && 
 
 # os.system("python app.py")
 
-# time.sleep(3)
+time.sleep(4)
 
-# os.system('start "" "http://127.0.0.1:5000/"')
+os.system('start "" "http://127.0.0.1:5000/"')
