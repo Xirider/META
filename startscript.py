f06f638349de246e7f4378618925ebc202180b28
Xirid <peter.k.albert@gmail.com>
2019-08-30 12:26:28 +0200

    Update startscript.py

--- startscript.py
+++ startscript.py
@@ -1,7 +1,7 @@
 import os
 import time
 import subprocess
-subprocess.run(r"activate qa && cd C:/Users/sophi/Desktop/peter/qa/inference && python app.py", shell=True)
+subprocess.run(r'activate qa && cd C:/Users/sophi/Desktop/peter/qa/inference && python app.py && start "" "http://127.0.0.1:5000/"', shell=True)
 # os.system("activate qa")
 # time.sleep(5)
 # exe_str = r"cd C:/Users/sophi/Desktop/peter/qa/inference"
