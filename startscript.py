00a736155ebc8d7044406a49c411197a9ced3eac
Xirid <peter.k.albert@gmail.com>
2019-08-30 12:04:34 +0200

    Update startscript.py

--- startscript.py
+++ startscript.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 os.system("activate qa")
 exe_str = r"cd C:/Users/sophi/Desktop/peter/qa/inference"
@@ -7,3 +7,7 @@ exe_str = r"cd C:/Users/sophi/Desktop/peter/qa/inference"
 os.system(exe_str)
 
 os.system("python app.py")
+
+time.sleep(3)
+
+os.system("start '' 'http://127.0.0.1:5000/'")
